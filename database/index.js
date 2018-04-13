6f1e878015b8bd91771ab37dbbd1b4d34fbf5518
Hack Reactor Student <student@hackreactor.com>
2018-04-12 18:58:23 -0700

    Implement Henry's code review feedback

--- database/index.js
+++ database/index.js
@@ -1,59 +1,62 @@
+const mongoose = require('mongoose');
 const sampleData = require('./reviewsSampleData.js');
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/reviews');
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
+db.once('open', () => {
+  console.log('connected to Mongoose');
 });
 
 const listingSchema = mongoose.Schema({
-    id: Number,
-    reviews: [
-        {
-            name: String,
-            profilePicture: String,
-            date: {type: Date, default: Date.now()},
-            body: String,
-            stars: {
-                overall: Number,
-                accuracy: Number, 
-                communication: Number, 
-                cleanliness: Number,
-                location: Number, 
-                checkIn: Number, 
-                value: Number
-            }
-        }
-    ],
-    numberOfReviews: Number,
+  id: { type: Number, unique: true },
+  reviews: [
+    {
+      name: String,
+      profilePicture: String,
+      date: { type: Date, default: Date.now() },
+      body: String,
+      stars: {
+        overall: Number,
+        accuracy: Number,
+        communication: Number,
+        cleanliness: Number,
+        location: Number,
+        checkIn: Number,
+        value: Number,
+      },
+    },
+  ],
+  numberOfReviews: Number,
 });
 
 const Listing = mongoose.model('Review', listingSchema);
 
-const addListings = function(listings) {
-    listings.forEach(function(listing, index) {
-        let newListing = new Listing(listing);
-        newListing.save(function(err, newListing) {
-            if (err) return console.log(err);
-        });
+const addListings = (listings) => {
+  listings.forEach((listing) => {
+    const newListing = new Listing(listing);
+    newListing.save((err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`The listing ${data} has been saved`);
+      }
     });
+  });
 };
 
-const getListings = function(id, cb) {
-    Listing.findOne({'id': id}, function(err, results) {
-        if (err) {
-            return handleError(err);
-        } else {
-            cb(results)
-        }
-    });
+const getListings = (id, cb) => {
+  Listing.findOne({ id }, (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      cb(results);
+    }
+  });
 };
 
 addListings(sampleData.createListings(100));
 
 module.exports = {
-    getListings
-}
+  getListings,
+};
--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 const dbHelper = require('../database/index.js');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static('../client/public'));
 
-app.get('/reviews/:id', (req, res) => (
-    dbHelper.getListings(req.params.id, function(results) {
+app.get('/reviews/:id', ({params}, res) => (
+    dbHelper.getListings(params.id, function(results) {
         res.send(results);
     })
 ));
 
-app.listen(3002, () => console.log('Example app listening on port localhost:3002'))
+app.listen(3002, () => console.log('Example app listening on port http://localhost:3002'))
