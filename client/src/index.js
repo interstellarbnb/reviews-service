fc4dedfd954d60c817a4e58a7dfa825f533fde26
Hack Reactor Student <student@hackreactor.com>
2018-04-13 11:53:16 -0700

    Added a note to myself about babel polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// if I want to use Promises, array.includes or other newer JavaScript methods, I need to
+// download polyfill and import it here.
+// Ask Bryce and Henry for help.
+// https://babeljs.io/docs/usage/polyfill/
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
